a11f88e469210b902035d8e56578f06bd3c925c1
emancuso <mancusoelyse@gmail.com>
2016-12-04 13:24:06 -0500

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wombats;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author riley
+ */
+public class Book {
+    private String title;
+    private String author;
+    private String genre;
+    private ArrayList<Review> reviews;
+    
+    
+    public String getTitle() {
+        return title;
+    }
+    
+    public String getAuthor() {
+        return author;
+    }
+    
+    public String getGenre() {
+        return genre;
+    }
+    
+    public ArrayList<Review> getReviews() {
+        return reviews;  
+    }
+    
+    public void setTitle(String newTitle) {
+        title = newTitle;
+    }
+    
+    public void setAuthor(String newAuthor) {
+        author = newAuthor;
+    }
+    
+    public void setGenre(String newGenre) {
+        genre = newGenre;
+    }
+    
+    public void addReview(Review newReview) {
+        reviews.add(newReview);
+    }
+}
