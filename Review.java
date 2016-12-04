cb9101e532b7705d290d098e7d87f69bd60fa21c
emancuso <mancusoelyse@gmail.com>
2016-12-04 13:33:48 -0500

    Add files via upload

---
+++ Review.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wombats;
+
+/**
+ *
+ * @author riley
+ */
+public class Review {
+    
+    private int rating;
+    private String content;
+    private int bookId;
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
+    }
+    
+}
